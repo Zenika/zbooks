ffcf5c11509985f87c70ef194cf02c152f465250
mmouterde <martin.mouterde@zenika.com>
2013-11-12 15:23:00 +0100

    git test

--- zBooksWeb/src/main/java/com/zenika/zbooks/web/HomeController.java
+++ zBooksWeb/src/main/java/com/zenika/zbooks/web/HomeController.java
@@ -9,7 +9,19 @@ public class HomeController {
 
 	@RequestMapping(value="/")
 	public ModelAndView home () {
+package com.zenika.zbooks.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/")
+	public ModelAndView home () {
         //git test
+        //git test      ssss
 		//git test2
 		return new ModelAndView("/WEB-INF/views/home.jsp");
 	}
