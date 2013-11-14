0d1addd7472e4d562bb4a8627305f3c53132cbe7
mmouterde <martin.mouterde@zenika.com>
2013-11-14 10:23:35 +0100

    home.html

---
+++ zBooksWeb/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html ng-app="zBooks">
+<head>
+    <meta charset="UTF-8">
+    <title>Zenika Books</title>
+    <script src="resources/js/angular.min.js"></script>
+    <script src="resources/js/homeController.js"></script>
+    <script src="resources/js/home.js"></script>
+
+
+    <link rel="stylesheet" href="resources/css/home.css">
+
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="page-header">
+    <h1><strong>z<span class="">Book888s</span></strong></h1>
+</div>
+</body>
+</html>
--- zBooksWeb/src/main/webapp/resources/js/homeController.js
+++ zBooksWeb/src/main/webapp/resources/js/homeController.js
@@ -1,3 +1,12 @@
 function HomeController ($scope, $routeParams, $http) {
 	$scope.message = $http.get("/user/coucou");
+    $scope.bookList = [
+    {isbn:"047094224X",title:"Professional NoSQL	John Wiley & Sons Ltd",lang:"EN"},
+    {isbn:"0596518846",title:"SQL in a Nutshell	O'Reilly",lang:"EN"},
+    {isbn:"2212136382",title:"HTML 5 : Une référence pour le développeur web	Eyrolles",lang:"FR"},
+    {isbn:"2744025828",title:"Javascript - Les bons éléments",lang:"FR"},
+    {isbn:"1449344852",title:"AngularJS	O'Reilly",lang:"EN"},
+    {isbn:"0131872486",title:"Thinking in Java	Prentice Hall",lang:"EN"},
+    {isbn:"210059446X",title:"Scrum - Le guide pratique de la méthode agile la plus populaire	Dunod",lang:"FR"}
+    ]
 }
