bcd7a6979c52441af5d990cb3b1f0a8e7c46959b
Sebastien Brousse <seb.brousse@gmail.com>
2014-01-13 08:49:07 +0100

    Add generic to collection in a unit test

--- zBooksWeb/src/test/java/com/zenika/zbooks/persistence/ZBooksMapperTest.java
+++ zBooksWeb/src/test/java/com/zenika/zbooks/persistence/ZBooksMapperTest.java
@@ -1,13 +1,8 @@
 package com.zenika.zbooks.persistence;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.HashSet;
-import java.util.List;
-
+import com.zenika.zbooks.UnitTest;
+import com.zenika.zbooks.entity.ZBook;
+import com.zenika.zbooks.entity.ZCollection;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -16,9 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.zenika.zbooks.UnitTest;
-import com.zenika.zbooks.entity.ZBook;
-import com.zenika.zbooks.entity.ZCollection;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
@@ -116,7 +112,7 @@ public class ZBooksMapperTest extends AbstractDBTest implements UnitTest {
     }
 
     private boolean checkIdUnicity(List<ZBook> zBooks) {
-        HashSet monHashSet = new HashSet();
+        HashSet<Integer> monHashSet = new HashSet<>();
         for (ZBook zbook : zBooks) {
             if (!monHashSet.add(zbook.getId()))
                 return false;
